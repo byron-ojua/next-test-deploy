b81d1e657347f9a547f02eb58497bf9bb06f9f34
1Run <byron.ojua@gmail.com>
2024-02-14 01:47:14 -0800

    Added firebase

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,43 +3,44 @@ import Link from "next/link";
 import { useAuthState } from 'react-firebase-hooks/auth'
 import { auth, db } from '@/app/firebase/config'
 import { useRouter } from 'next/navigation'
-import { collection, doc, getDocs, query, where } from 'firebase/firestore'
-import { getUserDoc } from '@/app/firebase/userUtil'
+// import { collection, doc, getDocs, query, where } from 'firebase/firestore'
+// import { getUserDoc } from '@/app/firebase/userUtil'
 
 export default function Home() {
   const [ user ] = useAuthState(auth)
   const router = useRouter()
-  // const userSession = sessionStorage.getItem('user')
+  // // const userSession = sessionStorage.getItem('user')
 
-  // if(!user && !userSession){
+  // // if(!user && !userSession){
   if(!user){
       router.push('/sign-in')
   }
 
 
-  async function getUserDoc() {
-    const usersRef = collection(db, "users");
-    const q = query(usersRef, where("uid", "==", user.uid));
-    const querySnapshot = await getDocs(q);
+  // async function getUserDoc() {
+  //   const usersRef = collection(db, "users");
+  //   const q = query(usersRef, where("uid", "==", user.uid));
+  //   const querySnapshot = await getDocs(q);
   
-    if (!querySnapshot.empty) {
-      const userDoc = querySnapshot.docs[0].data(); // Take the first document that matches
-      console.log("User data:", userDoc);
-      return userDoc;
-    } else {
-      console.log("No user found with that UID.");
-      return null;
-    }
-  }
+  //   if (!querySnapshot.empty) {
+  //     const userDoc = querySnapshot.docs[0].data(); // Take the first document that matches
+  //     console.log("User data:", userDoc);
+  //     return userDoc;
+  //   } else {
+  //     console.log("No user found with that UID.");
+  //     return null;
+  //   }
+  // }
 
-  var userDoc = getUserDoc()
-  var firstName = userDoc.firstName
+  // var userDoc = getUserDoc()
+  // var firstName = userDoc.firstName
 
 
 
   return (
     <main>
-      <h1>Hello,{getUserDoc().firstName}</h1>
+      {/* <h1>Hello,{getUserDoc().firstName}</h1> */}
+      <h1>Hello</h1>
       <Link href="/database">Database examples</Link>
       <br />
       <Link href="/sign-up">Example Sign-Up</Link>
